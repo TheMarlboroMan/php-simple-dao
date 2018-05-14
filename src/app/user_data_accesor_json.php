83c81b23a953d6bcf14bd0c5b92d3388e26d4ebb
The Marlboro Man <marlborometal@gmail.com>
2018-05-14 22:07:14 +0200

    Update user_data_accesor_json.php

--- src/app/user_data_accesor_json.php
+++ src/app/user_data_accesor_json.php
@@ -2,6 +2,13 @@
 namespace app;
 
 //!Extends the user_mock_da to write the storage to a json file with each change.
+//TODO: This is actually wrong: the user_data_accesor_json IS NOT a user_data_accesor_mock
+//and the abstraction breaks in many places: for example, access to the storage is needed
+//in the parent class and both a json and mock accessors would share the same storage.
+//What should actually happen here is that this is implemented in terms of the mock, and
+//so could have a mock object. That would not fix the sharing and providing of the container,
+//so maybe both of them should actually be implemented in terms of a third class: the 
+//container.
 class user_data_accesor_json extends user_data_accesor_mock implements \dao\data_accesor {
 
 	private $filename=null;
